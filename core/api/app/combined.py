7f700f1ca558740911ee3a6fcc8d963863dabafe
marcoscruuuuuz <marcos.augusto.br1@gmail.com>
2026-08-16 04:11:45 -0300

    P7: mount camera snapshot router

--- core/api/app/combined.py
+++ core/api/app/combined.py
@@ -1,6 +1,8 @@
 from .main import app
 from .platform import router as platform_router
 from .media import router as media_router
+from .camera_media import router as camera_media_router
 
 app.include_router(platform_router)
 app.include_router(media_router)
+app.include_router(camera_media_router)
